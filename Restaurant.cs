a0a01b00a40e89ef3a9236111f9741c7f2b5b91a
Varshith2516 <himavarshith5678@gmail.com>
2026-03-09 18:09:26 +0530

    Implement Restaurant class with command pattern

---
+++ Restaurant.cs
@@ -0,0 +1,38 @@
+using System;
+ 
+namespace FoodiesDelight
+
+{
+
+    public class Restaurant
+
+    {
+
+        private ICommand command;
+ 
+        public void AddCommand(ICommand command)
+
+        {
+
+            this.command = command;
+
+        }
+ 
+        public void ProcessCommand()
+
+        {
+
+            if (command != null)
+
+            {
+
+                command.Execute();
+
+            }
+
+        }
+
+    }
+
+}
+ 
